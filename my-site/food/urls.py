17a54620bb8d971bda2ebb5c4be1903e4ab38278
Ritesh Tiwari <tiwariritesh989@gmail.com>
2023-12-16 13:54:37 +0530

    create a user using frontend page and an extra field (email).

--- my-site/food/urls.py
+++ my-site/food/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('<int:item_id>/', views.detail, name="detail"),
     path('item/', views.item, name="item"),
+    # add items
+    path('add/', views.create_item, name="create_item"), 
+    # update items
+    path('update/<int:id>/', views.update_item, name="update_item"),
+
+    # delete items
+    path('delete/<int:id>/', views.delete_item, name="delete_item"),
+
 ]
