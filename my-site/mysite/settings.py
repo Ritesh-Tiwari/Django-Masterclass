546d1665b02eae2d3cd2fa90117e02c20aee5419
Ritesh Tiwari <tiwariritesh989@gmail.com>
2023-12-25 13:07:40 +0530

    using singnals

--- my-site/mysite/settings.py
+++ my-site/mysite/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL  = '/static/'
 LOGIN_REDIRECT_URL = 'food:index'
+LOGIN_URL   ='login'
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'pictures')
+MEDIA_URL   = '/pictures/'
