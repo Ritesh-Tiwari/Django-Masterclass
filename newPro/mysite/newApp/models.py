6a6ed61e5ded6dd43e61020e67fbb1ab54d1f2d0
Ritesh Tiwari <tiwariritesh989@gmail.com>
2023-12-26 17:56:18 +0530

    starting new project and name is eCommerce

--- newPro/mysite/newApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Movies(models.Model):
-    name    = models.CharField(max_length=250)
-    rating  = models.FloatField()
-
-    def __str__(self):
-        return self.name
