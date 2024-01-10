ea7fb1396159957133c9587a5ea6a16bbf38fa0d
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-01-10 18:27:11 +0530

    creating a post create method in posts app.

--- socialMedia/users/views.py
+++ socialMedia/users/views.py
@@ -4,7 +4,7 @@ from django.http.response import HttpResponse
 from .forms import LoginForm, UserRegistartionForm,UserEiditForm,ProfileEiditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-
+from posts.models import Post
 
 # Create your views here.
 def user_login(request):
@@ -31,7 +31,9 @@ def user_login(request):
 
 @login_required
 def index(request):
-    return render(request, 'users/index.html')
+    current_user = request.user
+    posts = Post.objects.filter(user=current_user)
+    return render(request, 'users/index.html',{'posts':posts})
 
 
 def register(request):
