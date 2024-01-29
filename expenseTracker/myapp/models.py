1c22b10e725c87f3e2844190c311ddd1d08eae46
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-01-29 14:00:52 +0530

    backup

--- expenseTracker/myapp/models.py
+++ expenseTracker/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Expense'
+        verbose_name_plural = 'Expenses'
