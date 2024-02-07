2da1e0aa87508aeeda7c14ece7b9a87f0499b0c4
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-02-07 13:34:51 +0530

    creating view and form for chatroom

--- chatApp/chatapp/views.py
+++ chatApp/chatapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import ChatRoom
 
 # Create your views here.
 def index(request):
-    return render(request, 'chatapp/index.html')
+    chatrooms = ChatRoom.objects.all()
+    context= {
+        'chatrooms':chatrooms,
+    }
+    return render(request, 'chatapp/index.html', context)
+
+def chatroom(request, slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    return render (request,'chatapp/room.html',{'chatroom':chatroom})
