808858367df966bdc4a23e0471f1274a1b9b7faa
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-02-08 17:16:37 +0530

    saving messages in databse and display on web page.

--- chatApp/chatapp/consumers.py
+++ chatApp/chatapp/consumers.py
@@ -1,6 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from . models import ChatRoom, ChatMessage
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -35,7 +37,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'room':room,
             }
         )
-    
+        await self.save_message(username, room, message)
+
+
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
@@ -45,3 +49,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'username':username,
             'room':room,
         }))
+
+
+        
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = ChatRoom.objects.get(slug=room)
+
+        ChatMessage.objects.create(user=user, room = room, message_content=message)
