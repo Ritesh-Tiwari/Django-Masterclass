956751852d394b1962867a6c6e2e3ac0bdc38b75
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-02-10 17:20:55 +0530

    creating payment method via stripe but this method is not working i will try nect time.

--- dMarket/myapp/views.py
+++ dMarket/myapp/views.py
@@ -27,12 +27,12 @@ def detail(request, id):
 
 @csrf_exempt
 def create_checkout_session(request,id):
-    request_data = json.load(request.body)
+    request_data = json.loads(request.body)
     product = Product.objects.get(id=id)
-    stripe.api_key =  settings.STRIPE_SECRET_KEY
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     checkout_session = stripe.checkout.Session.create(
         customer_email = request_data['email'],
-        payment_method_type = ['card'],
+        payment_method_types = ['card'],
         line_items=[
             {
                 'price_data':{
@@ -40,38 +40,44 @@ def create_checkout_session(request,id):
                     'product_data':{
                         'name':product.name,
                     },
-                    'unit_amount': int(product.price * 100)
+                    'unit_amount':int(product.price * 100)
                 },
                 'quantity':1,
             }
         ],
         mode='payment',
-        success_url = request.build_absolute_url(reverse('success'))+
-        '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url= request.build_absolute_url(reverse("failed")),
+        success_url = request.build_absolute_uri(reverse('success')) +
+        "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url = request.build_absolute_uri(reverse('failed')),
         
     )
-
+    
     order = Orderdetail()
     order.customer_email = request_data['email']
     order.product = product
     order.stripe_payment_intent = checkout_session['payment_intent']
     order.amount = int(product.price)
     order.save()
-
+    
     return JsonResponse({'session_id':checkout_session.id})
 
 def payment_success_view(request):
     session_id = request.GET.get('session_id')
     if session_id is None:
         return HttpResponseNotFound()
-    stripe.api_key= settings.STRIPE_SECRET_KEY 
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     session = stripe.checkout.Session.retrieve(session_id)
-    order = get_object_or_404(Orderdetail, stripe_payment_intent=session.payment_intent)
-    order.has_paid = True
+    order = get_object_or_404(Orderdetail,stripe_payment_intent= session.payment_intent)
+    order.has_paid=True
+    # updating sales stats for a product
+    product = Product.objects.get(id=order.product.id)
+    product.total_sales_amount = product.total_sales_amount + int(product.price)
+    product.total_sales = product.total_sales + 1
+    product.save()
+    # updating sales stats for a product
     order.save()
-
-    return render (request, 'myapp/payment_success.html',{'order':order})
+    
+    return render(request,'myapp/payment_success.html',{'order':order})
 
 def payment_failed_view(request):
     return render(request, 'myapp/failed.html')
